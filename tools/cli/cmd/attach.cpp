6a0e4462a502960907f21a24763cb9a82f9ab1dd
Sebastian Winkler <sebwink@pm.me>
2025-12-26 19:05:10 +0000

    Some cli stuff

--- tools/cli/cmd/attach.cpp
+++ tools/cli/cmd/attach.cpp
@@ -1,10 +1,25 @@
 #include "attach.hpp"
 
+#include <cstdio>
+
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+
 namespace bad::cli::cmd::attach {
 
 void run(const Config &config) {
   std::cout << "ATTACH" << std::endl;
   std::cout << "PID: " << config.pid << std::endl;
+  if (ptrace(PTRACE_ATTACH, config.pid, /*addr=*/nullptr, /*data=*/nullptr) <
+      0) {
+    std::perror(&"Could not attach to process with PID "[config.pid]);
+  }
+
+  int wait_status;
+  int options = 0;
+  if (waitpid(config.pid, &wait_status, options) < 0) {
+    std::perror("waitpid failed");
+  }
 }
 
 void setup(CLI::App &app) {
